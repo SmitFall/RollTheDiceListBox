c171955baeac17ec6c20fd8320caeea2c9d3acdb
Smitfall <captainfalconrsmith@gmail.com>
2022-04-21 08:53:19 -0600

    Add project files.

---
+++ RollTheDiceListBox/RollTheDiceListBox.Designer.vb
@@ -0,0 +1,134 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class RollTheDiceListBox
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.DisplayListBox = New System.Windows.Forms.ListBox()
+        Me.MenuStrip1 = New System.Windows.Forms.MenuStrip()
+        Me.MenuToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.HelpToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ToolTip1 = New System.Windows.Forms.ToolTip(Me.components)
+        Me.RollButton = New System.Windows.Forms.Button()
+        Me.ClearButton = New System.Windows.Forms.Button()
+        Me.ExitButton = New System.Windows.Forms.Button()
+        Me.MenuStrip1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'DisplayListBox
+        '
+        Me.DisplayListBox.BackColor = System.Drawing.SystemColors.Menu
+        Me.DisplayListBox.ColumnWidth = 20
+        Me.DisplayListBox.Font = New System.Drawing.Font("Cascadia Mono", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.DisplayListBox.FormattingEnabled = True
+        Me.DisplayListBox.ItemHeight = 20
+        Me.DisplayListBox.Location = New System.Drawing.Point(2, 36)
+        Me.DisplayListBox.MultiColumn = True
+        Me.DisplayListBox.Name = "DisplayListBox"
+        Me.DisplayListBox.RightToLeft = System.Windows.Forms.RightToLeft.No
+        Me.DisplayListBox.Size = New System.Drawing.Size(798, 324)
+        Me.DisplayListBox.TabIndex = 0
+        Me.ToolTip1.SetToolTip(Me.DisplayListBox, "Displays the dice roll outcome" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10))
+        '
+        'MenuStrip1
+        '
+        Me.MenuStrip1.ImageScalingSize = New System.Drawing.Size(20, 20)
+        Me.MenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.MenuToolStripMenuItem, Me.HelpToolStripMenuItem})
+        Me.MenuStrip1.Location = New System.Drawing.Point(0, 0)
+        Me.MenuStrip1.Name = "MenuStrip1"
+        Me.MenuStrip1.Size = New System.Drawing.Size(800, 30)
+        Me.MenuStrip1.TabIndex = 1
+        Me.MenuStrip1.Text = "MenuStrip1"
+        '
+        'MenuToolStripMenuItem
+        '
+        Me.MenuToolStripMenuItem.Name = "MenuToolStripMenuItem"
+        Me.MenuToolStripMenuItem.Size = New System.Drawing.Size(60, 26)
+        Me.MenuToolStripMenuItem.Text = "&Menu"
+        '
+        'HelpToolStripMenuItem
+        '
+        Me.HelpToolStripMenuItem.Name = "HelpToolStripMenuItem"
+        Me.HelpToolStripMenuItem.Size = New System.Drawing.Size(55, 26)
+        Me.HelpToolStripMenuItem.Text = "&Help"
+        '
+        'RollButton
+        '
+        Me.RollButton.Location = New System.Drawing.Point(401, 382)
+        Me.RollButton.Name = "RollButton"
+        Me.RollButton.Size = New System.Drawing.Size(125, 67)
+        Me.RollButton.TabIndex = 2
+        Me.RollButton.Text = "&Roll"
+        Me.ToolTip1.SetToolTip(Me.RollButton, "Click to roll the dice again")
+        Me.RollButton.UseVisualStyleBackColor = True
+        '
+        'ClearButton
+        '
+        Me.ClearButton.Location = New System.Drawing.Point(532, 382)
+        Me.ClearButton.Name = "ClearButton"
+        Me.ClearButton.Size = New System.Drawing.Size(125, 67)
+        Me.ClearButton.TabIndex = 3
+        Me.ClearButton.Text = "&Clear"
+        Me.ToolTip1.SetToolTip(Me.ClearButton, "Click to clear the display")
+        Me.ClearButton.UseVisualStyleBackColor = True
+        '
+        'ExitButton
+        '
+        Me.ExitButton.Location = New System.Drawing.Point(663, 382)
+        Me.ExitButton.Name = "ExitButton"
+        Me.ExitButton.RightToLeft = System.Windows.Forms.RightToLeft.Yes
+        Me.ExitButton.Size = New System.Drawing.Size(125, 67)
+        Me.ExitButton.TabIndex = 4
+        Me.ExitButton.Text = "&Exit"
+        Me.ToolTip1.SetToolTip(Me.ExitButton, "Click to exit program")
+        Me.ExitButton.UseVisualStyleBackColor = True
+        '
+        'RollTheDiceListBox
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.ExitButton)
+        Me.Controls.Add(Me.ClearButton)
+        Me.Controls.Add(Me.RollButton)
+        Me.Controls.Add(Me.DisplayListBox)
+        Me.Controls.Add(Me.MenuStrip1)
+        Me.MainMenuStrip = Me.MenuStrip1
+        Me.Name = "RollTheDiceListBox"
+        Me.RightToLeftLayout = True
+        Me.Text = "Roll the Dice"
+        Me.MenuStrip1.ResumeLayout(False)
+        Me.MenuStrip1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents DisplayListBox As ListBox
+    Friend WithEvents ToolTip1 As ToolTip
+    Friend WithEvents MenuStrip1 As MenuStrip
+    Friend WithEvents MenuToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents HelpToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents RollButton As Button
+    Friend WithEvents ClearButton As Button
+    Friend WithEvents ExitButton As Button
+End Class
---
+++ RollTheDiceListBox/RollTheDiceListBox.vb
@@ -0,0 +1,73 @@
+﻿'Fallon Smith
+'RCET0265
+'Spring 2022
+'Roll the Dice list box
+'
+
+Option Explicit On
+Option Strict On
+
+Public Class RollTheDiceListBox
+    Private Sub RollButton_Click(sender As Object, e As EventArgs) Handles RollButton.Click
+
+        Dim numbers(12) As Integer
+        Dim seperator As String = " |"
+        Dim columnLength As Integer
+        Dim lineSeperator As String = "-"
+        Dim LineLength As Integer = columnLength * (UBound(numbers) + 1)
+
+
+        For I = 1 To 1000
+            numbers(GetRandomInteger()) += 1
+        Next
+
+        Dim TopRow As String
+
+        For i = 1 To 12
+            TopRow &= CStr(i) & "|".PadLeft(4)
+        Next
+
+        DisplayListBox.Items.Add(TopRow)
+
+        'separator
+        DisplayListBox.Items.Add(StrDup(77, lineSeperator))
+        'display contentents of randomNumber() array
+        Dim BottomRow As String
+
+        For i = LBound(numbers) To UBound(numbers)
+
+            BottomRow &= CStr(numbers(i)) & seperator.PadLeft(3)
+
+
+        Next
+        DisplayListBox.Items.Add(BottomRow)
+
+        DisplayListBox.Items.Add(StrDup(77, lineSeperator))
+    End Sub
+
+
+
+    Function GetRandomInteger() As Integer
+        Dim value As Integer
+        Dim temp As Single
+
+        Randomize()
+        temp = (Rnd() * 12) + 1
+        value = CInt(System.Math.Floor(CDbl(temp)))
+        System.Math.Floor(CDbl(1))
+
+        Return value
+
+    End Function
+
+
+    Private Sub ExitButton_Click(sender As Object, e As EventArgs) Handles ExitButton.Click
+        'Closes the program
+        Me.Close()
+    End Sub
+
+    Private Sub ClearButton_Click(sender As Object, e As EventArgs) Handles ClearButton.Click
+        'Clears out the listbox
+        DisplayListBox.Items.Clear()
+    End Sub
+End Class
